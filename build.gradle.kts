0006958242f6aa6c64671376e2ccea7425f2627b
Tyler Bochard <zeruth09@gmail.com>
2025-08-13 12:11:54 -0400

    init 1.0.0

---
+++ build.gradle.kts
@@ -0,0 +1,20 @@
+plugins {
+    id("java")
+    kotlin("jvm") version "2.2.0"
+}
+
+group = "nullpops"
+version = "1.0.0"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
